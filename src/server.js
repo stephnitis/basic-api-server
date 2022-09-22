9009c117c3953dea8bd02b4195177b9c889de69a
Stephanie Hill <stephni.hill@gmail.com>
2022-09-21 20:44:21 -0700

    some tests passing

--- src/server.js
+++ src/server.js
@@ -3,12 +3,28 @@
 const express = require('express');
 const mountainsRouter = require('./routes/mountains.route');
 const parksRouter = require('./routes/parks.route');
+const logger = require('./middleware/logger');
+const notFound = require('./error-handlers/404');
+const errorHandler = require('./error-handlers/500');
 const PORT = process.env.PORT || 3002;
 
 const app = express();
 app.use(express.json());
 app.use(mountainsRouter);
 app.use(parksRouter);
+app.use(logger);
+
+app.get('/', (req, res, next) => {
+  res.status(200).send('hello');
+});
+
+app.get('/bad', (req, res, next) => {
+  next('this route is bad');
+});
+
+app.use('*', notFound);
+
+app.use(errorHandler);
 
 function start(){
   app.listen(PORT, () => console.log('listening on port', PORT));
