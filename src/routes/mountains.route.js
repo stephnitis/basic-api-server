4f389dce7b2a5f06f7171be75446fc48c7b7fae1
Stephanie Hill <stephni.hill@gmail.com>
2022-09-21 20:20:48 -0700

    get function

--- src/routes/mountains.route.js
+++ src/routes/mountains.route.js
@@ -5,11 +5,41 @@ const { MountainModel } = require('../models');
 
 const router = express.Router();
 
+router.get('/mountains', async (req, res, next) => {
+
+  const getMountains  = await MountainModel.findAll();
+  console.log(getMountains);
+  res.status(200).send(getMountains);
+
+});
+
+// router.get('/mountains', async (req, res, next) => {
+
+//   const getOneMountain  = await MountainModel.findOne({
+//     order: [
+//       ['name'],
+//       ['summit'],
+//       ['tripType'],
+//     ],
+//   });
+//   console.log(getOneMountain);
+//   res.status(200).send(getOneMountain);
+
+// });
+
 router.post('/mountains', async (req, res, send) => {
   console.log('mtn req body', req.body);
 
   const newMountain = await MountainModel.create(req.body);
   res.status(200).send(newMountain);
 });
+
+// router.delete('/mountains/:id', async (req, res) => {
+//   MountainModel.destroy({
+//     where: {
+//       id: req.params.id,
+//     },
+//   });
+// });
 
 module.exports = router;
--- src/routes/parks.route.js
+++ src/routes/parks.route.js
@@ -5,6 +5,14 @@ const { ParkModel } = require('../models');
 
 const router = express.Router();
 
+router.get('/parks', async (req, res, next) => {
+
+  const parks  = await ParkModel.findAll();
+  console.log(parks);
+  res.status(200).send(parks);
+
+});
+
 router.post('/parks', async (req, res, send) => {
   console.log('parks req body', req.body);
 
